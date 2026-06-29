310fc3323e5aeb6807cb11493c8217ef7bcb8b13
AghilanNS <aghilan2005@gmail.com>
2026-06-29 18:10:49 +0200

    Delete Staff.py

--- Staff.py
+++
@@ -1,9 +0,0 @@
-#Defining class Staff
-class Staff:
-    def __init__(self, name, job):
-        self.name = name
-        self.job = job
-
-    def display_info(self):
-        return f"Staff Name: {self.name}\nJob: {self.job}"
-            
